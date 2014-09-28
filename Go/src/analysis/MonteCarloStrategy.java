35bd0f5dadbe3e72a472cc378ed3f4af1a325677
Nick Vanderweit <nick.vanderweit@gmail.com>
2014-09-28 15:41:40 -0600

    Continue writing MCTS

--- Go/src/analysis/MonteCarloStrategy.java
+++ Go/src/analysis/MonteCarloStrategy.java
@@ -7,16 +7,25 @@ import java.util.Random;
 import game.Board;
 import game.Intersection;
 
-@SuppressWarnings("unused") // Still being actively worked on
+
 public final class MonteCarloStrategy implements Strategy {
 	private final Analyzer analyzer;
 	private final Random random;
-	private final int samples;
+	private final int iterations;
+	
+	/**
+	 * For testing purposes
+	 */
+	protected static interface TestingCallback {
+		void treeSize(int size);
+	}
+	
+	protected TestingCallback testingCallback = null;
 
-	private MonteCarloStrategy(Random random, Analyzer analyzer, int samples) {
+	private MonteCarloStrategy(Random random, Analyzer analyzer, int iterations) {
 		this.random = random;
 		this.analyzer = analyzer;
-		this.samples = samples;
+		this.iterations = iterations;
 	}
 	
 	public static final Analyzer DEFAULT_ANALYZER =
@@ -41,10 +50,18 @@ public final class MonteCarloStrategy implements Strategy {
 		public final GameTreeNode parent;
 		public final Board board;
 		
+		private int wins;
+		private int losses;
+		private int ties;
+		
 		private GameTreeNode(Board board, GameTreeNode parent) {
 			this.children = new ArrayList<>();
 			this.parent = parent;
 			this.board = board;
+			
+			this.wins = 0;
+			this.losses = 0;
+			this.ties = 0;
 		}
 		
 		public static GameTreeNode getInstance(Board board) {
@@ -54,36 +71,121 @@ public final class MonteCarloStrategy implements Strategy {
 		public static GameTreeNode getInstance(Board board, GameTreeNode parent) {
 			return new GameTreeNode(board, parent);
 		}
+
+		public void backpropagate(double score) {
+			GameTreeNode cursor = this;
+			
+			while(cursor != null) {
+				if(score == 0)
+					cursor.ties++;
+				else if(score > 0)
+					cursor.wins++;
+				else
+					cursor.losses++;
+				
+				cursor = cursor.parent;
+			}
+		}
+		
+		public int getScore() {
+			return wins - losses;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			this.toStringInner(0, sb);
+			return sb.toString();
+		}
+
+		private void toStringInner(int indent, StringBuilder sb) {
+			for(int i = 0; i < indent; i++) {
+				sb.append('\t');
+			}
+			
+			sb.append(String.format("%d / %d / %d\n", wins, ties, losses));
+			
+			for(GameTreeNode child : this.children) {
+				child.toStringInner(indent + 1, sb);
+			}
+		}
 	}
 	
 	private static final class GameTree {
 		public final List<GameTreeNode> nodes;
+		public final GameTreeNode root;
 		
-		private GameTree() {
+		private GameTree(GameTreeNode root) {
+			this.root = root;
 			this.nodes = new ArrayList<>();
+			this.nodes.add(root);
 		}
 		
-		public static GameTree getInstance() {
-			return new GameTree();
+		public static GameTree getInstance(GameTreeNode root) {
+			return new GameTree(root);
 		}
 		
-		public final GameTreeNode choose(Random random) {
-			return nodes.get(random.nextInt(nodes.size()));
+		public final GameTreeNode select(Random random) {
+			GameTreeNode node = root;
+			int me = root.board.currentPlayer;
+			
+			while(node.children.size() > 0) {
+				if(random.nextDouble() < 0.1)
+					break;
+				
+				if(node.board.currentPlayer == me) { // choose the most promising node for me
+					node = node.children.stream().max((a,b) -> a.getScore() - b.getScore()).get();
+				}
+				else { // Choose the most promising node for the other player
+					node = node.children.stream().min((a,b) -> a.getScore() - b.getScore()).get();
+				}
+			}
+			
+			return node;
 		}
 	}
 	
 	@Override
 	public Intersection findBestMove(Board board) {
-		int[][] intersection = board.intersections;
-		int size = board.boardSize;
+		GameTreeNode root = GameTreeNode.getInstance(board);
+		GameTree tree = GameTree.getInstance(root);
 		
-		GameTree tree = GameTree.getInstance();
-		tree.nodes.add(GameTreeNode.getInstance(board));
-		
-		for(int sample = 0; sample < samples; sample++) {
-			GameTreeNode node = tree.choose(random);
+		for(int iteration = 0; iteration < iterations; iteration++) {
+			if(testingCallback != null) {
+				testingCallback.treeSize(tree.nodes.size());
+			}
+			
+			// Select
+			GameTreeNode node = tree.select(random);
+			
+			// Expand
+			List<Intersection> moves = node.board.getMoves();
+			if(moves.size() == 0) continue;
+			Intersection move = moves.get(random.nextInt(moves.size()));
+			GameTreeNode newNode =
+					GameTreeNode.getInstance(node.board.makeMove(move.x, move.y), node);
+			node.children.add(newNode);
+			tree.nodes.add(newNode);
+			
+			// Simulate
+			double score = simulate(newNode.board);
+			
+			// Backpropagate
+			newNode.backpropagate(score);
 		}
 		
 		return null; // Pass
+	}
+
+	private double simulate(Board board) {
+		List<Intersection> moves = board.getMoves();
+		
+		for(int i = 0; (moves.size() > 0) && i < 199; i++) {
+			Intersection move = moves.get(random.nextInt(moves.size()));
+			board = board.makeMove(move.x, move.y);
+			moves = board.getMoves();
+		}
+
+		return analyzer.analyze(board.currentPlayer, board);
 	}
 }
--- Go/test/analysis/MonteCarloStrategyTest.java
+++ Go/test/analysis/MonteCarloStrategyTest.java
@@ -9,6 +9,7 @@ import java.util.Random;
 
 import org.junit.Test;
 import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class MonteCarloStrategyTest {
@@ -35,5 +36,38 @@ public class MonteCarloStrategyTest {
 				MonteCarloStrategy.DEFAULT_ITERATIONS);
 		
 		assertNull(mcs.findBestMove(board));
+	}
+	
+	/**
+	 * Every iteration in MCTS, the explored game tree should obtain
+	 * one new node.
+	 */
+	@Test
+	public void treeGrowsEveryIteration() {
+		Board board = new Board(19, 0);
+		
+		MonteCarloStrategy mcs = MonteCarloStrategy.getInstance(
+				random,
+				MonteCarloStrategy.DEFAULT_ANALYZER,
+				MonteCarloStrategy.DEFAULT_ITERATIONS);
+		
+		Reference<Integer> oldSize = new Reference<>(-1);
+		
+		mcs.testingCallback = new MonteCarloStrategy.TestingCallback() {
+			@Override
+			public void treeSize(int size) {
+				assertTrue(oldSize.contents < size);
+				oldSize.contents = size;
+			}
+		};
+		
+		mcs.findBestMove(board);
+	}
+	
+	private static class Reference<T> {
+		public T contents;
+		public Reference(T contents) {
+			this.contents = contents;
+		}
 	}
 }
